2fbb260eaaeb4e7e7ee27b26f20f4c17bf9a0081
caliente-cxd <18629244952@163.com>
2020-05-23 16:10:37 +0800

    Site updated: 2020-05-23 16:10:32

---
+++ js/click_show_text.js
@@ -0,0 +1,34 @@
+<script src="https://cdn.staticfile.org/jquery/3.1.1/jquery.min.js"></script>
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        var a = new Array
+        ("爱你","爱你","爱你","爱你");
+        var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 5,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+           "color": "rgb(" + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + ")"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+			3000,
+			function() {
+			    $i.remove();
+			});
+    });
+    setTimeout('delay()', 2000);
+});
+
+function delay() {
+    $(".buryit").removeAttr("onclick");
+}
